8391fd1edff0c24d66eeea1106d9d52c8864bc7f
breadjune <breadjune@breadjune.local>
2022-06-29 21:59:06 +0900

    [add] trafficboss adm 프로젝트 생성

---
+++ src/main/java/kr/co/trafficboss/tbadm/TbadmApplication.java
@@ -0,0 +1,13 @@
+package kr.co.trafficboss.tbadm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbadmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbadmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/trafficboss/tbadm/TbadmApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.trafficboss.tbadm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbadmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
